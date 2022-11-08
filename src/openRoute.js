d70aab960ac60060ecc448c62ae3496ea200b36a
Nikhil Vyas <nikhilvyas771@gmail.com>
2022-11-08 15:23:21 +0530

    Created header component UI

--- src/openRoute.js
+++ src/openRoute.js
@@ -2,14 +2,18 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import SignUp from "./pages/signUp";
 import Login from "./pages/login";
+import Header from "./components/sharedComponent/header";
+import Footer from "./components/sharedComponent/footer";
 
 const OpenRoutes = () => {
   return (
     <>
+      <Header />
       <Routes>
         <Route path="/signup" element={<SignUp />} />
-        <Route path="/" element={<Login />} /> 
+        <Route path="/" element={<Login />} />
       </Routes>
+      <Footer />
     </>
   );
 };
